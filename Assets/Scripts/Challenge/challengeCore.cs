78efa2faf97b5aa9e4552a024dacfeea8b9fe15f
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2020-07-28 22:28:20 +0700

    update

--- Assets/Scripts/Challenge/challengeCore.cs
+++ Assets/Scripts/Challenge/challengeCore.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class challengeCore : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class challengeCore : MonoBehaviour {
+
+    private Challenge challenge;
+
+    public Text challengeName;
+    public Text challengeDes;
+    
+    private Challenge ReadChallenge() {
+        StreamReader r = new StreamReader(Application.dataPath + "\\challenge.json");
+        string json = r.ReadToEnd();
+        Challenge res = JsonUtility.FromJson<Challenge>(json);
+        return res;
+    }
+    void Start() {
+
+        //request challenge from server
+
+        //request challenge from server
+
+        challenge = ReadChallenge();
+
+        challengeName.text = challenge.challengeName;
+        challengeDes.text = challenge.challengeDescription;
+
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
