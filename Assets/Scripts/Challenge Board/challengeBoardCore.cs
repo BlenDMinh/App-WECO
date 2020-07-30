383aa69aeff21dd3f8fdaecaf5a8efe1ea7ff7a7
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2020-07-30 20:55:35 +0700

    update

--- Assets/Scripts/Challenge Board/challengeBoardCore.cs
+++ Assets/Scripts/Challenge Board/challengeBoardCore.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class challengesBoardCore : MonoBehaviour {
-    
+public class challengeBoardCore : MonoBehaviour {
     public Button challengeButton;
     public Transform buttonParent;
     public RectTransform challengesContent;
     void Start() {
+
+        //Generate buttons
         float curY = 665 * buttonParent.parent.parent.transform.localScale.y;
 
         //Read all challenges in to List<Challenge> challenge
@@ -20,15 +23,21 @@ public class challengesBoardCore : MonoBehaviour {
 
         challengesContent.sizeDelta = new Vector2(challengesContent.rect.width, System.Math.Abs(665 - 300 * challenge.Count - (365 + 150)) + 150);
 
-        foreach (Challenge c in challenge) {
+        for(int i = 0; i < challengeFilePath.Length; i++) {
+            Challenge c = challenge[i];
             curY -= 300 * buttonParent.parent.parent.transform.localScale.y;
             Button button = Instantiate(challengeButton, buttonParent);
             button.transform.SetParent(buttonParent);
             button.transform.position = new Vector2(0, curY);
+            button.GetComponentInChildren<Text>().text = c.challengeName;
+            string path = challengeFilePath[i];
+            button.onClick.AddListener(delegate { ToChallenge(path); });
         }
-        
     }
-    void Update() {
-        
+
+    void ToChallenge(string challengePath) {
+        Debug.Log(challengePath);
+        File.Copy(challengePath, Application.dataPath + "\\challenge.json", true);
+        SceneManager.LoadScene("Challenge");
     }
 }
