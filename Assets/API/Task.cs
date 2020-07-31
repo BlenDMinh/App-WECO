c32c2b03ecf96354cd76e5ba16530c86a3e0b6dc
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2020-07-31 11:06:12 +0700

    update

--- Assets/API/Task.cs
+++ Assets/API/Task.cs
@@ -1,3 +1,8 @@
-﻿public class Task {
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Task {
+	public string taskName, taskDescription;
 	public Reward reward;
+	public float x, y;
 }
