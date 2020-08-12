6849ae5d38ac66443c0c1179c56eac19f19270d6
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2020-08-12 19:52:20 +0700

    update

--- Assets/Scripts/Task/exitButton.cs
+++ Assets/Scripts/Task/exitButton.cs
@@ -4,7 +4,29 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class exitButton : MonoBehaviour {
-    public void exit() {
-        SceneManager.UnloadSceneAsync("Task");
+
+    public GameObject canvas;
+    [SerializeField]
+    private float delayTime;
+    private float time;
+    private string scenename;
+    private void Start() {
+        time = 0f;
+        scenename = "";
+    }
+    public void exit(string name) {
+        Animator animator = canvas.GetComponent<Animator>();
+        if (animator != null)
+            animator.SetBool("isExit", true);
+        scenename = name;
+    }
+
+    private void Update() {
+        if (scenename == "")
+            return;
+        time += Time.deltaTime;
+        if (time < delayTime)
+            return;
+        SceneManager.UnloadSceneAsync(scenename);
     }
 }
--- Assets/Scripts/Task/taskCore.cs
+++ Assets/Scripts/Task/taskCore.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,17 +9,23 @@ public class taskCore : MonoBehaviour {
     public GameObject reqContainer;
     public GameObject reqBox;
     public Text taskDesc;
+    public Button confirmButton;
 
-    private float curY;
+    private Task task;
+    private List<Slider> reqList;
+
     void Start() {
+        float curY, off;
+        off = reqContainer.GetComponent<RectTransform>().anchoredPosition.y;
         curY = reqContainer.GetComponent<RectTransform>().sizeDelta.y;
-        Task task = Task.LoadCurrentTask();
+        task = Task.LoadCurrentTask();
         taskDesc.text = "\n" + task.taskDescription;
 
         foreach(KeyValuePair<string, int> req in task.requirement) {
+            confirmButton.interactable = false;
             GameObject obj = Instantiate(reqBox, reqContainer.transform);
             RectTransform r = obj.GetComponent<RectTransform>();
-            r.anchoredPosition = new Vector2(0, curY);
+            r.anchoredPosition = new Vector2(0, curY + off);
             GameObject minVal = obj.transform.Find("minVal").gameObject;
             GameObject maxVal = obj.transform.Find("maxVal").gameObject;
             GameObject unit = obj.transform.Find("unit").gameObject;
@@ -29,8 +36,21 @@ public class taskCore : MonoBehaviour {
             unit.GetComponent<Text>().text = req.Key;
             slider.maxValue = req.Value;
 
-            curY += r.sizeDelta.y;
-            reqContainer.GetComponent<RectTransform>().sizeDelta = new Vector2(reqContainer.GetComponent<RectTransform>().sizeDelta.x, curY);
+            reqList.Add(slider);
+
+            curY -= r.sizeDelta.y;
+            reqContainer.GetComponent<RectTransform>().sizeDelta = new Vector2(reqContainer.GetComponent<RectTransform>().sizeDelta.x, -curY);
         }
+    }
+
+    private void Update() {
+        bool isAccept = true;
+        foreach (Slider slider in reqList)
+            if (slider.value < slider.maxValue) {
+                isAccept = false;
+                break;
+            }
+        if (isAccept)
+            confirmButton.interactable = true;
     }
 }
