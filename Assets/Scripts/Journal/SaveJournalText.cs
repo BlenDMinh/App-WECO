deb20452e080cbe148a7047b51e5f53a1f9f95f5
Nguyễn Thành Danh <danh23072004@users.noreply.github.com>
2020-08-12 17:57:56 +0700

    Update new Journal scene

--- Assets/Scripts/Journal/SaveJournalText.cs
+++ Assets/Scripts/Journal/SaveJournalText.cs
@@ -8,10 +8,15 @@ public class SaveJournalText : MonoBehaviour
     // Everything relate to save journal
     // This script is approached to JournalField to get input, and SaveJournal to save input
     // JournalField object (Unity) = journalInput (code)
-    public Text journalInput;
+    private UserData data = new UserData();
     public void ButtonClick_SaveJournal()
     {
-        UserData data = new UserData();
-        data.SaveUserJournal(journalInput.text);
+
+    }
+    public void LoadJournal()
+    {
+        data = UserData.LoadUserData();
+        this.GetComponent()
+
     }
 }
