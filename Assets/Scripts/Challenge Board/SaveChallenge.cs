b145ff2d1c3e7ac93d6eadf39054fb16b657d317
Nguyễn Thành Danh <danh23072004@users.noreply.github.com>
2020-11-07 22:13:28 +0700

    Update SaveChallenge (mostly done)
    Add scroll view into the SaveChallenge list

---
+++ Assets/Scripts/Challenge Board/SaveChallenge.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveChallenge : MonoBehaviour
+{
+    public GameObject container;
+    public GameObject template;
+
+    private UserData user;
+    public void LoadChallengeInfo()
+    {
+        user = UserData.LoadUserData();
+        List<Challenge> challengeList = Challenge.LoadSaveChallenge();
+
+        int i = 0;
+        float templateHigh = 40f;
+
+        template.gameObject.SetActive(true);
+
+        foreach (var challenge in challengeList)
+        {
+            GameObject challengeRow = Instantiate(template, container.transform);
+            RectTransform challengeRowRectTransform = challengeRow.GetComponent<RectTransform>();
+            challengeRowRectTransform.anchoredPosition = new Vector2(0, 150 - templateHigh * i);
+
+            challengeRow.transform.GetChild(0).GetComponent<Text>().text = challenge.challengeName;
+            challengeRow.transform.GetChild(1).GetComponent<Text>().text = challenge.challengeDescription;
+
+            if (i % 2 == 0)
+            {
+                challengeRow.GetComponent<Image>().color = new Color32(197, 236, 128, 255);
+            }
+            else
+            {
+                challengeRow.GetComponent<Image>().color = new Color32(233, 211, 135, 255);
+            }
+
+            challengeRow.gameObject.SetActive(true);
+            i++;
+        }
+    }
+}
---
+++ Assets/Scripts/Journal/CaretBlinking.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CaretBlinking : MonoBehaviour
+{
+    public Text TextObject;
+    public GameObject Cursor;
+    public float CursorDelay = 0.3f;
+    private Vector3 CursorOffset = new Vector3(10, 10);
+
+    private string _msg = "";
+    private float _timer = 0f;
+
+    private void Update()
+    {
+        //update msg
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                if (_msg.Length != 0)
+                {
+                    _msg = _msg.Substring(0, _msg.Length - 1);
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                _msg += "\r\n";
+            }
+            else
+            {
+                _msg += c;
+            }
+        }
+
+        TextObject.text = _msg;
+
+        //place cursor
+        var gen = TextObject.cachedTextGenerator;
+        if (gen.verts.Count > 0)
+        {
+            var v = gen.verts[gen.verts.Count - 1].position;
+            v = TextObject.transform.TransformPoint(v);
+            Cursor.transform.position = v + CursorOffset;
+        }
+
+        //do blink
+        _timer += Time.deltaTime;
+        if (_timer > CursorDelay)
+        {
+            _timer = 0f;
+            Cursor.SetActive(!Cursor.activeSelf);
+        }
+    }
+}
