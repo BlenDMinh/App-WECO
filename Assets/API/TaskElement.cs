a635a8ef693841b3efd2b3eda455c3fb159a43f8
Henrily041103 <anhmonkdv@gmail.com>
2021-01-14 22:32:58 +0700

    TaskBar test 1
    TODO: display_task (popup window dunno how to :disappointed: )
    may or may not be buggy

--- Assets/API/TaskElement.cs
+++ Assets/API/TaskElement.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TaskElement : MonoBehaviour {
     //===============BlenDMinh===========DO NOT TOUCH===========//
@@ -9,7 +10,7 @@ public class TaskElement : MonoBehaviour {
     //selection = true if this TaskElement is selected
     private bool selection = false;
 
-    //List of task difficulty user can choose (currently string)
+    //List of task difficulties user can choose (currently string)
     private List<string> taskDifficulties;
 
     //Number of fish receive;
@@ -57,11 +58,51 @@ public class TaskElement : MonoBehaviour {
     }
 
     //===============Monk======================================//
+    
+    private Sprite type; //the type of challenge ie: plastic bottles, plastic bags...
+    public int i = 0;
+
+    private Sprite GetType() {
+    	return this.type;
+    }
+
+    private void SetType(Sprite newtype) {
+    	this.type = newtype;
+    }
+
     private void InitTaskElement() {
         //Display TaskElement on Unity
         //Examples: set 'reward' as Text to view on a Unity scene
         //...
 
+    	//Canvas taskbox = GetComponents<Canvas>()[1]; //main canvas
+    	Text[] texts = GetComponents<Text>(); //3 text: text 0, difficulty 1, reward 2
+    	Image[] images = GetComponents<Image>(); //2 sprites: feesh 0, type 1
+
+    	texts[1].text = GetTaskDifficulties()[0];
+    	texts[2].text = reward.ToString(); //reward
+    	
+    	images[1].sprite = type;
+    }
+    
+    public void DisplayTask(){
+    	//Display tasks according to difficulty
+    }
+    
+    public void TaskBar_Selected(Canvas can){
+    	//Change TaskBar appearance when it is selected 
+    	selection = true;
+    	Image img = can.GetComponent<Image>();
+    	img.color = UnityEngine.Color.cyan;
+    }
+    
+    public void Change_Difficulty(Text t){
+    	//Change the difficulty of the selected challenge. Cycle through the diffs
+    	i++;
+    	t.text = GetTaskDifficulties()[i];
+    	if (i == 2){
+    		i = 0;
+    	}
     }
     //===============Monk=====================================//
 
