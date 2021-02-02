9cc5a693f23c5d55803f197a12f459ee7dd9b69e
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2021-02-02 17:17:50 +0700

    fix ChallengeBoard + update TaskElement prefab

--- Assets/Scripts/Challenge Board/challengeBoardCore.cs
+++ Assets/Scripts/Challenge Board/challengeBoardCore.cs
@@ -6,33 +6,32 @@ using UnityEngine.SceneManagement;
 using Newtonsoft.Json;
 
 public class challengeBoardCore : MonoBehaviour {
-    public Button challengeButton;
-    public RectTransform challengesContent;
+
+    [SerializeField]
+    private Button challengeButton;
+    [SerializeField]
+    private RectTransform challengesContent;
     void Start() {
         //Generate buttons
         //Read all challenges in to List<Challenge> challenge
         List<string> challenge = new List<string>();
-        float buttonHeight = challengeButton.GetComponent<RectTransform>().rect.height;
         challenge = JsonConvert.DeserializeObject<List<string>>((Resources.Load("challengeList") as TextAsset).ToString());
 
-        challengesContent.anchoredPosition = new Vector2(0, 0);
-        challengesContent.sizeDelta = new Vector2(challengesContent.rect.width, buttonHeight * challenge.Count);
-        float curY = buttonHeight;
-
         for(int i = 0; i < challenge.Count; i++) {
-            curY -= buttonHeight;
             Button button = Instantiate(challengeButton, challengesContent);
             button.transform.SetParent(challengesContent);
-            button.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, curY);
+
             button.GetComponent<Image>().sprite = Resources.Load<Sprite>("challengeTexture\\" + challenge[i] + "_button");
             button.GetComponent<Image>().type = Image.Type.Simple;
             button.GetComponent<Image>().preserveAspect = true;
+
             string challengeName = challenge[i];
             if(challenge[i] != "Coming soon")
                 button.onClick.AddListener(delegate { ToChallenge(challengeName); });
         }
     }
 
+    // Apply script to all buttons
     void ToChallenge(string challengeName) {
         string writeData = (Resources.Load("challenges\\" + challengeName) as TextAsset).text;
         if (Application.platform == RuntimePlatform.Android) {
@@ -43,9 +42,5 @@ public class challengeBoardCore : MonoBehaviour {
             File.WriteAllText(Application.dataPath + "//Data//challenge.json", writeData);
         }
         SceneManager.LoadScene("Journey");
-    }
-
-    private void Update() {
-        Debug.Log(challengesContent.anchoredPosition.y);
     }
 }
