406c0b3d5731f461ab36ee7f2bfa0f0ba068ec1d
BlenDMinh <53397490+BlenDMinh@users.noreply.github.com>
2021-04-16 19:06:30 +0700

    Community

--- Assets/API/postElement.cs
+++ Assets/API/postElement.cs
@@ -4,22 +4,19 @@ using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-	public class PostElement {
-        public Image userAvatar { get; set; } 
-        public Image postImage { get; set; }
-        public string userName { get; set; } 
-        public string postText { get; set; } 
-        public string Address { get; set; } 
+public class PostElement : MonoBehaviour {
+    public Image userAvatar { get; set; } 
+    public Image postImage { get; set; }
+    public string userName { get; set; } 
+    public string postText { get; set; } 
+    public string Address { get; set; } 
 
-        public PostElement loadAddress(string address){
-            Address = address;
-	        StreamReader r = new StreamReader(Address);
-			string json = r.ReadToEnd();
-			PostElement res = JsonConvert.DeserializeObject<PostElement>(json);
-			r.Close();
-			return res;
-        }
+    public PostElement loadAddress(string address){
+        Address = address;
+	    StreamReader r = new StreamReader(Address);
+		string json = r.ReadToEnd();
+		PostElement res = JsonConvert.DeserializeObject<PostElement>(json);
+		r.Close();
+		return res;
     }
 }
