fd643b5316d1ac1baf443dfc959d14293d2c0801
Alexander <k9514753@gmail.com>
2021-10-05 06:58:46 +0700

    Delete asset

--- Assets/Scripts/Community/Community_sub/GroupManager/GroupInfo.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-
-public class GroupInfo {
-    public string ID;
-    public string name;
-    List<string> member_uids;
-}
--- Assets/Scripts/Journey/toProgressButton.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class toProgressButton : MonoBehaviour {
-	public void to(string name) {
-		SceneManager.LoadScene("Progress", LoadSceneMode.Additive);
-	}
-}
